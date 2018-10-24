2145ffb4369e5bfc89805a7865f84c5a4038904e
Adrián Carro <adrian.carro@gmail.com>
2018-10-24 14:21:53 +0100

    Implement whole geography inheritance
    This new design replaces the previous inheritance within the job region of the deceased household. Now, any other alive household in the whole geography of regions can be chosen as heir

--- src/main/java/housing/Demographics.java
+++ src/main/java/housing/Demographics.java
@@ -68,12 +68,36 @@ public class Demographics {
                     iterator.remove();
                     totalPopulation--;
                     // Inheritance
-                    // TODO: This imposes inheritance within the same region, which is the job region of the dead household!!!
-                    h.transferAllWealthTo(region.households.get(rand.nextInt(region.households.size())));
+                    h.transferAllWealthTo(findRandomHousehold());
                 }
             }
         }
 	}
+
+    /**
+     * Choose a random household within the whole geography of regions, i.e., a random household from a random region
+     * with the probability of choosing a region being proportional to its population
+     */
+    private Household findRandomHousehold() {
+        // Draw a random integer from 0 to (totalPopulation - 1)
+        int randInt = rand.nextInt(totalPopulation);
+        // Set counter to zero
+        int counter = 0;
+        // First, iterate over the regions to find the chosen one (the one containing the chosen household)
+        Iterator<Region> regionIterator = geography.getRegions().iterator();
+        Region region = regionIterator.next();
+        while (counter + region.households.size() < randInt) {
+            counter += region.households.size();
+            region = regionIterator.next();
+        }
+        // Then, iterate over the households of that region until the chosen one is found
+        Iterator<Household> householdIterator = region.households.iterator();
+        Household household = householdIterator.next();
+        for (int i = 1; i < (randInt - counter); i++) { // Start at 1, since first household has already been called
+            householdIterator.next();
+        }
+        return household;
+    }
 
     //----- Getter/setter methods -----//
 
