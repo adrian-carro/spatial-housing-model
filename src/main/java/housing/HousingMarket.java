96f0c628cadc2c43e0a3c6ff5800cdfbbca4b05a
Adrián Carro <adrian.carro@gmail.com>
2019-05-20 19:51:25 +0100

    Set non-zero minimum probability of success at geometric distribution
    Zero probabilities, likely for large numbers of households, lead to an error, as zero is beyond the support of the geometric distribution.

--- src/main/java/housing/HousingMarket.java
+++ src/main/java/housing/HousingMarket.java
@@ -157,6 +157,8 @@ public abstract class HousingMarket {
                     // consecutive bids having at least a week between them
                     // TODO: Check this an exponential form, why not (1 - 7/30)^(nBids - 1)?
                     pSuccessfulBid = Math.exp(-nBids*config.derivedParams.MONTHS_UNDER_OFFER);
+                    // Set a minimum non-zero value to comply with the support of the geometric distribution, (0, 1]
+                    pSuccessfulBid = Math.max(pSuccessfulBid, Double.MIN_VALUE);
                     // Given the previous probability of success (two consecutive bids more than a week apart), find the
                     // number of attempts before a success (number of consecutive bids less than a week apart before two
                     // consecutive bids more than a week apart), which corresponds to a draw from a geometric
